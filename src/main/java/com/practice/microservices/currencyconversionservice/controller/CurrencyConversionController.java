3af9b64125fcafbdca9a4bdf5009c429b736ea46
Kanav Ghai <kanghai@publicisgroupe.net>
2023-10-03 16:14:26 +0530

    chore: setup distributed tracing

--- src/main/java/com/practice/microservices/currencyconversionservice/controller/CurrencyConversionController.java
+++ src/main/java/com/practice/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -3,6 +3,9 @@ package com.practice.microservices.currencyconversionservice.controller;
 import com.practice.microservices.currencyconversionservice.dto.CurrencyConversionBean;
 import com.practice.microservices.currencyconversionservice.proxy.CurrencyExchangeServiceProxy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,11 +16,22 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
+@Configuration(proxyBeanMethods = false)
+class RestTemplateConfiguration {
+    @Bean
+    RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+}
+
 @RestController
 public class CurrencyConversionController {
 
     @Autowired
     private CurrencyExchangeServiceProxy proxy;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @GetMapping("/currency-convertor")
     public CurrencyConversionBean getConvertedAmount(@RequestParam String from, @RequestParam String to, @RequestParam BigDecimal amount) {
@@ -25,7 +39,7 @@ public class CurrencyConversionController {
         Map<String, String> uriVariables = new HashMap<>();
         uriVariables.put("from", from);
         uriVariables.put("to", to);
-        ResponseEntity<CurrencyConversionBean> responseEntity = new RestTemplate()
+        ResponseEntity<CurrencyConversionBean> responseEntity = restTemplate
                 .getForEntity(
                         "http://localhost:8000/currency-exchange?from={from}&to={to}",
                         CurrencyConversionBean.class,
